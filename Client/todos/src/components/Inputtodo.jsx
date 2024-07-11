cd65198b7637de7ba3bb749f5c633de9b9632f95
Sriram.T <144159691+kuttysri07@users.noreply.github.com>
2024-07-11 13:34:11 +0530

    Update Inputtodo.jsx

--- Client/todos/src/components/Inputtodo.jsx
+++ Client/todos/src/components/Inputtodo.jsx
@@ -8,7 +8,7 @@ const Inputtodo = () => {
         e.preventDefault()
         try {
             const body={description};
-            const response=await fetch("http://localhost:5000/todos",{
+            const response=await fetch("/todos",{
                 method:"POST",
                 headers:{"Content-Type":"application/json"},
                 body:JSON.stringify(body)
